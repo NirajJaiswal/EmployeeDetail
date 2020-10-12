db7a7ed62101f1dcd0ec7275ddd8c6ca6c0cfdf2
shreya <shreyashrey096@gmail.com>
2020-10-13 00:49:52 +0530

    initializing value

--- app/src/main/java/com/example/employeedetail/LoginActivity.java
+++ app/src/main/java/com/example/employeedetail/LoginActivity.java
@@ -2,13 +2,60 @@ package com.example.employeedetail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+private TextView mEmailTextView;
+private TextView mPasswordTextView;
+private EditText mEmailEditText;
+private  EditText mPasswordEditText;
+private TextView mForgotTextView;
+private TextView mAccountTextView;
+private TextView mSignTextView;
+private Button mLoginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initView();
+
+
+
+    }
+
+    private void initView() {
+        mEmailTextView=findViewById(R.id.tv_email);
+        mEmailEditText=findViewById(R.id.et_email);
+        mPasswordTextView=findViewById(R.id.tv_password);
+        mPasswordEditText=findViewById(R.id.et_password);
+        mForgotTextView=findViewById(R.id.tv_forgot);
+        mAccountTextView=findViewById(R.id.tv_account);
+        mSignTextView=findViewById(R.id.tv_sign);
+        mLoginButton=findViewById(R.id.btn_login);
+
+
+
+
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+       mSignTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+               startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/employeedetail/RegisterActivity.java
+++ app/src/main/java/com/example/employeedetail/RegisterActivity.java
@@ -3,12 +3,59 @@ package com.example.employeedetail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+private TextView mRegisterName;
+
+    private TextView mRegisterMobile;
+    private TextView mRegisterEmail;
+    private TextView mRegisterPassword;
+    private TextView mRegisterAccount;
+    private TextView mRegisterSign;
+    private EditText mRegisterNameEditText;
+    private EditText mRegisterMobileEditText;
+    private EditText mRegisterEmailEditText;
+    private EditText mRegisterPasswordEditText;
+    private Button mRegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initView();
+    }
+
+    private void initView() {
+
+        mRegisterName=findViewById(R.id.tv_name_register);
+        mRegisterEmail=findViewById(R.id.tv_name_register);
+        mRegisterPassword=findViewById(R.id.tv_password_register);
+        mRegisterAccount=findViewById(R.id.tv_account_register);
+mRegisterSign=findViewById(R.id.tv_sign_register);
+         mRegisterNameEditText=findViewById(R.id.et_name_register);
+         mRegisterMobileEditText=findViewById(R.id.et_mobile_no);
+         mRegisterEmailEditText=findViewById(R.id.et_email_register);
+        mRegisterPasswordEditText=findViewById(R.id.et_password_register);
+       mRegisterButton=findViewById(R.id.btn_register);
+
+
+       mRegisterAccount.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+           }
+       });
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
