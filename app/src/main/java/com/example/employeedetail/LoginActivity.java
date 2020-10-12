0a2c5dfaf666ad28617c59b1d8ac8ddd0eae887a
Niraj <nirajjaiswal1996@gmail.com>
2020-10-13 02:06:57 +0530

    Login and Registration implementation

--- app/src/main/java/com/example/employeedetail/LoginActivity.java
+++ app/src/main/java/com/example/employeedetail/LoginActivity.java
@@ -1,61 +1,128 @@
 package com.example.employeedetail;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-private TextView mEmailTextView;
-private TextView mPasswordTextView;
-private EditText mEmailEditText;
-private  EditText mPasswordEditText;
-private TextView mForgotTextView;
-private TextView mAccountTextView;
-private TextView mSignTextView;
-private Button mLoginButton;
+    private TextView mEmailTextView;
+    private TextView mPasswordTextView;
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private TextView mAccountTextView;
+    private TextView mSignTextView;
+    private Button mLoginButton;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         initView();
-
+        setValue();
 
 
     }
 
-    private void initView() {
-        mEmailTextView=findViewById(R.id.tv_email);
-        mEmailEditText=findViewById(R.id.et_email);
-        mPasswordTextView=findViewById(R.id.tv_password);
-        mPasswordEditText=findViewById(R.id.et_password);
-        mForgotTextView=findViewById(R.id.tv_forgot);
-        mAccountTextView=findViewById(R.id.tv_account);
-        mSignTextView=findViewById(R.id.tv_sign);
-        mLoginButton=findViewById(R.id.btn_login);
-
-
-
-
-
+    private void setValue() {
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                String email=mEmailEditText.getText().toString().trim();
+                String password=mPasswordEditText.getText().toString().trim();
+                if(TextUtils.isEmpty(email))
+                {
+                    mEmailEditText.setError("Enter email");
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    mPasswordEditText.setError("Enter password");
+                    return;
+                }
+                if(password.length()<6)
+                {
+                    mPasswordEditText.setError("Password not less than 6 character");
+                    return;
+                }
+                signUser(email,password);
+                progressBar.setVisibility(View.VISIBLE);
+
             }
         });
 
-       mSignTextView.setOnClickListener(new View.OnClickListener() {
+        mSignTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
-               startActivity(intent);
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
+    }
+
+    private void initView() {
+        mEmailTextView = findViewById(R.id.tv_email);
+        mEmailEditText = findViewById(R.id.et_email);
+        mPasswordTextView = findViewById(R.id.tv_password);
+        mPasswordEditText = findViewById(R.id.et_password);
+        mAccountTextView = findViewById(R.id.tv_account);
+        mSignTextView = findViewById(R.id.tv_sign);
+        mLoginButton = findViewById(R.id.btn_login);
+        progressBar=findViewById(R.id.login_progress);
+        auth=FirebaseAuth.getInstance();
+
+
+    }
+
+    private void signUser(String email, String password) {
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if(task.isSuccessful())
+                {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(LoginActivity.this, "Successfully Login", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(),StartActivity.class));
+                    finish();
+                }
+                else
+                {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(LoginActivity.this, "No User Found", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+        if(user !=null)
+        {
+            startActivity(new Intent(getApplicationContext(),StartActivity.class));
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/employeedetail/RegisterActivity.java
+++ app/src/main/java/com/example/employeedetail/RegisterActivity.java
@@ -1,61 +1,114 @@
 package com.example.employeedetail;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
-private TextView mRegisterName;
-
-    private TextView mRegisterMobile;
-    private TextView mRegisterEmail;
-    private TextView mRegisterPassword;
-    private TextView mRegisterAccount;
     private TextView mRegisterSign;
     private EditText mRegisterNameEditText;
     private EditText mRegisterMobileEditText;
     private EditText mRegisterEmailEditText;
     private EditText mRegisterPasswordEditText;
     private Button mRegisterButton;
+    private ProgressBar mProgressBar;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         initView();
+        setValue();
     }
 
-    private void initView() {
-
-        mRegisterName=findViewById(R.id.tv_name_register);
-        mRegisterEmail=findViewById(R.id.tv_name_register);
-        mRegisterPassword=findViewById(R.id.tv_password_register);
-        mRegisterAccount=findViewById(R.id.tv_account_register);
-mRegisterSign=findViewById(R.id.tv_sign_register);
-         mRegisterNameEditText=findViewById(R.id.et_name_register);
-         mRegisterMobileEditText=findViewById(R.id.et_mobile_no);
-         mRegisterEmailEditText=findViewById(R.id.et_email_register);
-        mRegisterPasswordEditText=findViewById(R.id.et_password_register);
-       mRegisterButton=findViewById(R.id.btn_register);
 
 
-       mRegisterAccount.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-
-           }
-       });
+    private void setValue() {
+        mRegisterSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
 
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                String email=mRegisterEmailEditText.getText().toString().trim();
+                String password=mRegisterPasswordEditText.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    mRegisterEmailEditText.setError("Enter email");
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    mRegisterPasswordEditText.setError("Enter password");
+                    return;
+                }
+                if(password.length()<6)
+                {
+                    mRegisterPasswordEditText.setError("Password not less than 6 character");
+                    return;
+                }
+                registerUser(email,password);
+                mProgressBar.setVisibility(View.VISIBLE);
+
+
+
             }
         });
+    }
+
+    private void registerUser(String email, final String password) {
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    mProgressBar.setVisibility(View.GONE);
+                    Toast.makeText(RegisterActivity.this, "Successfully register", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(),StartActivity.class));
+                    finish();
+                }
+                else
+                {
+                    mProgressBar.setVisibility(View.GONE);
+                    Toast.makeText(RegisterActivity.this, "Some thing went Wrong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void initView() {
+        mRegisterSign = findViewById(R.id.tv_sign_register);
+        mRegisterNameEditText = findViewById(R.id.et_name_register);
+        mRegisterMobileEditText = findViewById(R.id.et_mobile_no);
+        mRegisterEmailEditText = findViewById(R.id.et_email_register);
+        mRegisterPasswordEditText = findViewById(R.id.et_password_register);
+        mRegisterButton = findViewById(R.id.btn_register);
+        mProgressBar=findViewById(R.id.progress);
+        auth=FirebaseAuth.getInstance();
 
     }
 }
